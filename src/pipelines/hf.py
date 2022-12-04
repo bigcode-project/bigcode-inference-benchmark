0947688aca02e3ccc859178de7aa69b24187b2a8
mayank31398 <mayank31398@gmail.com>
2022-12-05 03:24:41 +0530

    device map

--- src/pipelines/hf.py
+++ src/pipelines/hf.py
@@ -9,10 +9,11 @@ class HF_Pipeline(Pipeline):
     def __init__(self, args: Namespace, device: str = "cpu") -> None:
         super().__init__(args)
 
-        model_kwargs = {"device_map": "auto"}
+        model_kwargs = {}
 
         if args.dtype == torch.int8:
             model_kwargs["load_in_8bit"] = True
+            model_kwargs["device_map"] = "auto"
         else:
             model_kwargs["torch_dtype"] = args.dtype
 
