227ba2753036c0c40fb50b2c441fc3672a01a846
mayank31398 <mayank31398@gmail.com>
2022-12-12 04:15:58 +0530

    new loops

--- run_batch_size.sh
+++ run_batch_size.sh
@@ -7,7 +7,26 @@ do
     make $1 batch_size=$bs
 done
 
-for i in {1..40}
+# split for loops
+for i in {1..20}
+do
+    bs=$(($i*128))
+    make $1 batch_size=$bs
+done
+
+for i in {21..40}
+do
+    bs=$(($i*128))
+    make $1 batch_size=$bs
+done
+
+for i in {41..60}
+do
+    bs=$(($i*128))
+    make $1 batch_size=$bs
+done
+
+for i in {61..80}
 do
     bs=$(($i*128))
     make $1 batch_size=$bs
