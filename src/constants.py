d7fe3dd5f1aeead5d6d0041b59544f1cb30b7c2d
mayank31398 <mayank31398@gmail.com>
2023-01-29 21:59:15 +0530

    move constants

---
+++ src/constants.py
@@ -0,0 +1,13 @@
+NUM_GENERATED_TOKENS = "num_generated_tokens"
+TOKENIZE_TIME = "tokenize_time"
+MODEL_TIME = "model_time"
+DECODE_TIME = "decode_time"
+END_TO_END_TIME = "end_to_end_time"
+
+METRIC_KEYS = (
+    NUM_GENERATED_TOKENS,
+    TOKENIZE_TIME,
+    MODEL_TIME,
+    DECODE_TIME,
+    END_TO_END_TIME,
+)
